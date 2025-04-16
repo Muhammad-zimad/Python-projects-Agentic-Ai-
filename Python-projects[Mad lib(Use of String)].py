6bb959b66f497e0a33ec3a94920060260ef15062
Muhammad zimad <muhammadzimad156@gmail.com>
2025-04-17 01:29:49 +0500

    Add files via upload

---
+++ Python-projects[Mad lib(Use of String)].py
@@ -0,0 +1,17 @@
+# some way of use f {string}
+
+youtuber = "" # some string variable
+
+youtuber = "Islamic_World2k25"
+
+print("Subscribe to " + youtuber)
+print("Subscribe to {}".format(youtuber))
+print(f"Subscribe to {youtuber}")
+
+day = input("days: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+verb3 = input("verb3: ")
+madlib = f"Islamic_World2k25 is my Youtube channel Which i create {day} days ago , I Uploded Islamic {verb1} My {verb2}! i can earn to much money from {verb3}."
+
+print(madlib)
